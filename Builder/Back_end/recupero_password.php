ac10ee393377e6c22a4dc7a13cfb6614871d5c07
Matteo_Desa <mt.desalve@libero.it>
2024-05-09 15:50:16 +0200

    ciao

---
+++ Builder/Back_end/recupero_password.php
@@ -0,0 +1,44 @@
+<?php
+// Connessione al database SQLite3
+$db = new SQLite3('C:\xampp\htdocs\Progetto_palestra\builderschedule\DB_palestra\Gym_Db.db');
+
+// Verifica se è stata inviata una richiesta POST
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email-recover'])) {
+    $email = $_POST['email-recover'];
+
+    // Esempio di query per verificare se il CF è presente nel database
+    $query = $db->prepare("SELECT CF FROM Utente_registrato WHERE Email = :email");
+    $query->bindValue(':email', $email, SQLITE3_TEXT);
+    $result = $query->execute();
+
+    if ($result->fetchArray(SQLITE3_ASSOC)) {
+        echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
+                echo '<script>';
+                echo 'document.addEventListener("DOMContentLoaded", function() {';
+                echo '    Swal.fire({
+                            title: "trovato stupido",
+                            text: "Ora puoi diduuuu.",
+                            icon: "success",
+                            timer: 1500, // Tempo in millisecondi (2 secondi)
+                            showConfirmButton: false
+                            }).then(() => {
+                            window.location.href = "../Front_end/home.php";
+                            });';
+                echo '});';
+                echo '</script>';
+                exit;
+    } else {
+        // CF non trovato nel database, restituisci una risposta JSON di errore
+        echo json_encode(["success" => false]);
+    }
+
+    // Chiudi la connessione al database
+    $db->close();
+}
+
+// Se è stata inviata una richiesta POST per aggiornare la password
+
+
+
+?>
+
---
+++ Builder/Front_end/js/home.js
@@ -0,0 +1,98 @@
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+const registerLink = document.getElementById('register-link');
+const loginLink = document.getElementById('login-link');
+const cfInput = document.getElementById('cf-register');
+const emailInput = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordInput = document.getElementById('password-register');
+const togglePassword = document.getElementById('togglePassword');
+const togglePasswordd = document.getElementById('togglePassword-');
+
+const recoverLink = document.getElementById('recover-link');
+const recoverForm = document.getElementById('recover-form');
+const cfRecover = document.getElementById('cf-recover');
+
+registerLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'block';
+    recoverForm.style.display='none';
+});
+
+loginLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    loginForm.style.display = 'block';
+    registerForm.style.display = 'none';
+    recoverForm.style.display='none';
+});
+
+recoverLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'none';
+    recoverForm.style.display='block';
+});
+
+
+
+
+// Aggiungi event listener per l'input sui campi
+cfInput.addEventListener('input', validateCF);
+cfRecover.addEventListener('input', validateCF);
+emailInput.addEventListener('input', validateEmail);
+passwordInput.addEventListener('input', validatePassword);
+
+function validateCF() {
+    const cfValue = cfInput.value;
+    const cfRegex = /^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/;
+
+    if (!cfRegex.test(cfValue)) {
+        cfInput.setCustomValidity('Il codice fiscale non è valido.');
+    } else {
+        cfInput.setCustomValidity('');
+    }
+}
+
+function validateEmail() {
+    const emailValue = emailInput.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(emailValue)) {
+        emailInput.setCustomValidity('L\'email non è valida.');
+    } else {
+        emailInput.setCustomValidity('');
+    }
+}
+
+function validatePassword() {
+    const passwordValue = passwordInput.value;
+
+    if (passwordValue.length < 6 || !/[A-Z]/.test(passwordValue)) {
+        passwordInput.setCustomValidity('La password deve essere lunga almeno 6 caratteri e contenere almeno una lettera maiuscola.');
+    } else {
+        passwordInput.setCustomValidity('');
+    }
+}
+
+
+
+togglePassword.addEventListener('click', function() {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+
+    // Cambia l'icona per mostrare/nascondere la password
+    togglePassword.classList.toggle('fa-eye-slash');
+    togglePassword.classList.toggle('fa-eye');
+});
+
+
+togglePasswordd.addEventListener('click', function() {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    // Cambia l'icona per mostrare/nascondere la password
+    togglePasswordd.classList.toggle('fa-eye-slash');
+    togglePasswordd.classList.toggle('fa-eye');
+});
+/// recover pw///
